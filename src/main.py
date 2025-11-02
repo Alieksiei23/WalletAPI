17ba3c37d00f37093f6e0efbcf1e84a09fcc4024
Alieksiei <lesha.sidor57@gmail.com>
2025-11-02 13:31:22 +0300

    created docs, added monitoring and logging

--- src/main.py
+++ src/main.py
@@ -1,22 +1,12 @@
-from contextlib import asynccontextmanager
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from fastapi import FastAPI
 from src.api.v1 import analytics_router, transaction_router, user_router
-from src.core.config import engine
-from src.db.db_models import Base
 
 
-async def create_db_and_tables() -> None:
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+app = FastAPI()
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> None:
-    await create_db_and_tables()
-    yield
-
-app = FastAPI(lifespan=lifespan)
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(user_router)
 app.include_router(analytics_router)
